b5e54944286cbbb01f5c5e6754be6b0d1889012c
Torben Nielsen <torben.nielsen@space-inventor.com>
2026-09-03 11:04:00 +0200

    Fix formatting issues in csp_init.c

--- src/csp_init.c
+++ src/csp_init.c
@@ -19,10 +19,10 @@ csp_conf_t csp_conf = {
 	.hostname = "",
 	.model = "",
 	.revision = "",
-	.conn_dfl_so = CSP_O_NONE,
-	.dedup = CSP_DEDUP_OFF
 	.date = "",
 	.time = "",
+	.conn_dfl_so = CSP_O_NONE,
+	.dedup = CSP_DEDUP_OFF
 };
 
 void csp_init(void) {
